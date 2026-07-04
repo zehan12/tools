835e0122e0c8c0d7ff1e45c63874108e11c6de7b
zehan12 <zehan9211@gmail.com>
2026-07-05 02:11:05 +0530

    feat: :ocean: integrate Tailwind CSS v4 into the project via Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
