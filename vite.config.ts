ed49f83f06d7ed0a5c930b5b5db4a8b500a8ee66
zehan12 <zehan9211@gmail.com>
2026-07-05 02:16:46 +0530

    feat: :bamboo: integrate shadcn/ui and add cn utility function for tailwind styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import path from "path"
 import tailwindcss from '@tailwindcss/vite'
 import babel from '@rolldown/plugin-babel'
 
@@ -10,4 +11,9 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
