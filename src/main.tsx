3c9607c1abce8dae3e832f400dc9cf3ef426b9f7
zehan12 <zehan9211@gmail.com>
2026-07-06 02:17:23 +0530

    fix: 🐛 explicit i18n index import to resolve Vite HMR cache issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './i18n'
+import './i18n/index'
 
 createRoot(document.getElementById('root')!).render(
   <App />
